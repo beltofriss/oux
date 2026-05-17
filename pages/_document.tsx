76b311fcd4de0dfced3ee7017c85644d344aae7e
oprom0004 <rohrstaras@gmail.com>
2026-05-17 14:22:59 +0800

    chore: add google-site-verification meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="16x16" href="/oux/favicon-16x16.png" />
         <link rel="icon" href="/oux/favicon.svg" type="image/svg+xml" />
         <link rel="shortcut icon" href="/oux/favicon-32x32.png" />
+        <meta name="google-site-verification" content="vljRRmq3OONiFR43yFjMW0Rv1FQDsuv7Svxj-2zExoc" />
         <script defer src="https://cloud.umami.is/script.js" data-website-id="bd4538e6-5aba-44dc-843f-9448e7246402"></script>
       </Head>
       <body>
